13faaefbe9857e0b354b0afa2bf002335897765a
zenpa1 <zer0zenpa1@gmail.com>
2025-06-26 16:15:02 +0800

    Added comment router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # -- FastAPI Instance and Routes --
 from fastapi import FastAPI  # Core app
 from app import models, database  # SQLAlchemy table definitions and database functionality
-from .routers import posts, users  # For including routers
+from .routers import posts, users, comments  # For including routers
 
 # Create all tables if not existing
 models.Base.metadata.create_all(bind=database.engine)
@@ -13,6 +13,7 @@ app = FastAPI()
 # Setup routers
 app.include_router(posts.router)
 app.include_router(users.router)
+app.include_router(comments.router)
 
 # Handle dependencies
 
